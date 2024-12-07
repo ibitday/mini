5a707a920140ff5e4e55e973df8b33abb15f1ec9
JoyCood <joy.cood@gmail.com>
2024-12-07 14:13:49 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import {useEffect} from 'react';
-import { backButton } from '@telegram-apps/sdk-react';
+import {init,  backButton } from '@telegram-apps/sdk-react';
 import './App.css';
 
 function App() {
+  init()
+  
   useEffect(() => {
     backButton.show();
     return () => {
       backButton.hide();
     };
   }, [])
-  
+
   return (
       <h1 className="text-3xl font-bold underline text-red-600">
         Simple React Typescript Tailwind Sample
